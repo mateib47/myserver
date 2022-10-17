dd4692af40c54687d2d1caf41f68c582846f97ec
mateib47 <m.bucur@student.utwente.nl>
2022-10-17 23:51:18 +0900

    disable cors for contact post request

--- src/main/java/com/myserver/contact/ContactController.java
+++ src/main/java/com/myserver/contact/ContactController.java
@@ -2,17 +2,15 @@ package com.myserver.contact;
 
 import com.myserver.person.PersonRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/contact")
 @AllArgsConstructor
 public class ContactController {
     private ContactService contactService;
-
+//fix enable cors and make a proxy
+    @CrossOrigin
     @PostMapping(path = "add")
     public String addContact(@RequestBody ContactRequest contactRequest){
         return contactService.addContact(contactRequest);
